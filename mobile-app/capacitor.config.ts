649051567d7d2922155ba60a2213d0c333e3f130
gwaves <gwaves@vip.sina.com>
2026-06-21 14:01:36 +0800

    Fix Android mobile status bar layout

--- mobile-app/capacitor.config.ts
+++ mobile-app/capacitor.config.ts
@@ -5,7 +5,8 @@ const config: CapacitorConfig = {
   appName: 'Novel Reader',
   webDir: 'dist',
   server: {
-    androidScheme: 'https',
+    androidScheme: 'http',
+    cleartext: true,
   },
 }
 
