6777a61f1aaef829b6929c1435c1efd9e7ce8e87
gwaves <gwaves@vip.sina.com>
2026-06-23 09:16:46 +0800

    chore: distinguish mobile TTS apk

--- mobile-app/capacitor.config.ts
+++ mobile-app/capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli'
 
 const config: CapacitorConfig = {
-  appId: 'com.gwaves.novelreader.mobile',
-  appName: 'Novel Reader',
+  appId: 'com.gwaves.novelreader.tts',
+  appName: 'Novel TTS',
   webDir: 'dist',
   server: {
     androidScheme: 'http',
